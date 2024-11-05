8f8aae1d4027b5e65e1433a3f15fcb24213d0b5c
FatihTheG <fatihtheg123@gmail.com>
2024-11-05 18:26:54 +0100

    Fixed css file structure, added responsiveness to the homepage and registration page and some scss for registration

--- image_effects.js
+++ image_effects.js
@@ -8,7 +8,11 @@ function imageToggle() {
       images[i].style.height = "90%";
       images[i].style.transition = "1900ms";
     }
+    else if (document.body.style.width <= "1000px") {
+      images[i].style.width = "500px";
+      images[i].style.height = "300px";
 
+    }
     else {
       images[i].style.width = "500px";
       images[i].style.height = "300px";
