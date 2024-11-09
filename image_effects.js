59c3a19af40df828546f07f2dc1c0cb7abc8a41b
FatihTheG <fatihtheg123@gmail.com>
2024-11-09 22:49:13 +0100

    Removed redundant code in image_effects.js and updated index.html

--- image_effects.js
+++ image_effects.js
@@ -8,11 +8,7 @@ function imageToggle() {
       images[i].style.height = "90%";
       images[i].style.transition = "1900ms";
     }
-    else if (document.body.style.width <= "1000px") {
-      images[i].style.width = "500px";
-      images[i].style.height = "300px";
 
-    }
     else {
       images[i].style.width = "500px";
       images[i].style.height = "300px";
