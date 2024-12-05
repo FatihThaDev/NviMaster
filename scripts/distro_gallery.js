8ffcb683ef3894c3769df9a85af99d03e38c75db
FatihTheG <fatihtheg123@gmail.com>
2024-12-05 21:58:16 +0100

    Added a image gallery with images opening in modal

--- scripts/distro_gallery.js
+++ scripts/distro_gallery.js
@@ -11,3 +11,25 @@ menu.addEventListener("click", () => {
   }
 }
 )
+
+
+let modal = document.getElementById("modal");
+let modalImg = document.getElementById("modal-img");
+let scalarValue = document.getElementById("minmax-range").value;
+
+const showModal = src => {
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
+  modalImg.src = src;
+}
+
+const closeModal = () => {
+  modal.classList.remove('flex');
+  modal.classList.add('hidden');
+}
+
+const scaleModal = () => {
+  console.log("Scalar value is: " + scalarValue);
+  modalImg.classList.remove('max-w-[800px]');
+  modalImg.classList.add(`max-w-[${scalarValue}px]`);
+}
